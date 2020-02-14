389bc0434d29837dea1fe2e307f9bd5e5009b415
Joseph S. Cha <josephshincha@gmail.com>
2020-02-14 02:07:24 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,6 +2,7 @@ def nyc_pigeon_organizer(data)
   # write your code here!
   pigeons_list = {}
   date.each do |attributes, value|
-    value.each do |attribute_value, all_names|
-      all_names.each do |name|
+    value.each do |attribute_value, pigeon_names|
+      pigeon.each do |name|
+        pigeons_list[name] ||
 end
